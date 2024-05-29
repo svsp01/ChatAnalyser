1759800c55d4b4f0f137b4daa7d23da499ac8fea
Sakthi-kprinfo <sakthivignesh.shanmugam@kprinfo.com>
2024-05-29 17:38:48 +0530

    https config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+  },
 })
